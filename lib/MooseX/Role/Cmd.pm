e6733017a9f0a4cca19c2cced244bdbbe790d70c
Ian Sillitoe <isillitoe@cpan.org>
2008-11-06 13:13:24 +0000

    Import of ISILLITOE/MooseX-Role-Cmd-0.03 from CPAN.
    gitpan-cpan-distribution: MooseX-Role-Cmd
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         ISILLITOE/MooseX-Role-Cmd-0.03.tar.gz
    gitpan-cpan-author:       ISILLITOE
    gitpan-cpan-maturity:     released

--- lib/MooseX/Role/Cmd.pm
+++ lib/MooseX/Role/Cmd.pm
@@ -5,8 +5,9 @@ use warnings;
 
 use IPC::Cmd ();
 use Moose::Role;
+use MooseX::Role::Cmd::Meta::Attribute::Trait;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -130,16 +131,12 @@ sub run {
     if ( !( $full_path = IPC::Cmd::can_run($cmd) ) ) {
         confess qq{couldn't find command '$cmd'};
     }
-
-    # exclude this role's attributes from the flag list
-    # could use custom metaclasses and introspection, but this will do for now
-    my %non_flag = map { $_ => 1 } __PACKAGE__->meta->get_attribute_list;
-    my @flag_attrs = grep { !$non_flag{$_} } $self->meta->get_attribute_list;
-    my @flags = map { ( "-$_" => $self->$_ ) } @flag_attrs;
-
-    $cmd = [ $full_path, @flags, @args ];
+    
+    # build full list of cmd args from attrs
+    @args = $self->cmd_args( @args );
+    
     my ( $success, $error_code, $full_buf, $stdout_buf, $stderr_buf ) =
-      IPC::Cmd::run( command => $cmd );
+      IPC::Cmd::run( command => [ $full_path, @args ] );
 
     if ( !$success ) {
         confess "error running '$full_path': " . $error_code;
@@ -148,6 +145,103 @@ sub run {
     $self->stdout($stdout_buf);
     $self->stderr($stderr_buf);
     return 1;
+}
+
+=head2 cmd_args
+
+Returns a list of the computed arguments that will be added to the command
+
+=cut
+
+sub cmd_args {
+    my ( $self, @extra_args ) = @_;
+
+    # exclude this role's attributes from the flag list
+    # could use custom metaclasses and introspection, but this will do for now
+    my %non_flag   = map  {    $_ => 1     } __PACKAGE__->meta->get_attribute_list;
+    my @flag_attrs = grep { !$non_flag{$_} }       $self->meta->get_attribute_list;
+    
+    #####
+    # IS: 2008/10/15
+    # Changed the following to make a start on the suggestion above...
+    #####
+    #my @flags = map { ( "-$_" => $self->$_ ) } @flag_attrs;
+    my @flags = map { ( $self->_attr_name_to_cmd_options( $_ ) ) } @flag_attrs;
+
+    my @args = ( @flags, @extra_args );
+
+    return wantarray ? @args : \@args;
+}
+
+
+=head1 PRIVATE METHODS
+
+=head2 _attr_name_to_cmd_options
+
+Returns an array (or array reference) of command options that correspond
+to the given attribute name. This essentially provides the following
+functionality:
+
+Bool as flags, everything else as key => value params
+
+    has 'v'       => ( isa => 'Bool' );                 # -v
+    has 'i'       => ( isa => 'Str' );                  # -i input.data
+
+Use Getopt::Long conventions with hyphenations:
+
+    has 'v'       => ( isa => 'Bool' );                 # -v
+    has 'verbose' => ( isa => 'Bool' );                 # --verbose
+
+Use optional info from CmdOpt trait:
+
+    has 'v'       => ( isa          => 'Bool' );        # -v
+    
+    has 'short'   => ( traits       => [ 'CmdOpt' ],
+                       isa          => 'Bool',
+                       cmdopt_prefix => '-'
+                     );                                 # -short
+    
+    has 'rename'  => ( traits       => [ 'CmdOpt' ],
+                       isa          => 'Bool',
+                       cmdopt_name  => '+foo'
+                     );                                 # +foo
+
+=cut
+
+sub _attr_name_to_cmd_options {
+    my ( $self, $attr_name ) = @_;
+    
+    my $attr = $self->meta->get_attribute( $attr_name );
+    
+    # decide the default settings
+    my $opt_prefix = length( $attr_name ) == 1 ? '-' : '--';
+    my $opt_name   = $attr_name;
+    
+    # override defaults with Traits
+    if ( $attr->does('MooseX::Role::Cmd::Meta::Attribute::Trait') ) {
+        if ($attr->has_cmdopt_prefix) {
+            $opt_prefix = $attr->cmdopt_prefix;
+        }
+        if ($attr->has_cmdopt_name) {
+            $opt_prefix = '';                   # name overrides prefix
+            $opt_name   = $attr->cmdopt_name;
+        }
+    }
+    
+    # create the full option name
+    my $opt_fullname = $opt_prefix . $opt_name;
+    
+    my @options = ();
+    if ( $attr->type_constraint->is_a_type_of( 'Bool' ) ) {
+        push @options, ( $opt_fullname )
+            if $self->$attr_name;                       # only add if attr is true
+    }
+    else {
+        push @options, ( $opt_fullname, $self->$attr_name )
+            if defined $self->$attr_name;               # only add if attr is defined
+    }
+    
+    return wantarray ? @options : \@options;
 }
 
 =head1 AUTHOR
